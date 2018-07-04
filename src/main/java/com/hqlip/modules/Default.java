60a9f8b22e1617a36a5137519afe2e2cd3020573
hqlip <hqlip>
2018-07-04 15:31:57 +0800

    edit configuration & jstl

--- src/main/java/com/hqlip/modules/Default.java
+++ src/main/java/com/hqlip/modules/Default.java
@@ -1,6 +1,8 @@
 package com.hqlip.modules;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -11,12 +13,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/test")
 public class Default {
 	
-	@RequestMapping("/ModelAndVIew")
+	@RequestMapping("/ModelAndView")
 	public ModelAndView index() {
+		
+		List<Object> li = new ArrayList<>();
+		
+		for (int i = 0, len = 50; i < len; i++) {
+			Map o = new HashMap();
+			o.put("qq", "qq:" + i);
+			li.add(o);
+		}
 		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/ModelAndView.jsp");
-		mav.addObject("name", "hqlip");
+//		mav.addObject("name", "hqlip");
+		mav.addObject("qqs", li);
 		
 		return mav;
 	}
