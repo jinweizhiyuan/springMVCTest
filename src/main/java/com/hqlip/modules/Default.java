cb493d7cbcd78d177b571c1b97b1364748fddf26
hqlip <hqlip>
2018-07-09 11:06:16 +0800

    fix

--- src/main/java/com/hqlip/modules/Default.java
+++ src/main/java/com/hqlip/modules/Default.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,8 +15,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/test")
 public class Default {
 	
+	private static Logger log = LoggerFactory.getLogger(Default.class);
+	
 	@RequestMapping("/ModelAndView")
 	public ModelAndView index() {
+		
+		log.debug("===========ModlAndView");
 		
 		List<Object> li = new ArrayList<>();
 		
@@ -22,6 +28,7 @@ public class Default {
 			Map o = new HashMap();
 			o.put("qq", "qq:" + i);
 			li.add(o);
+			log.warn(i + "");
 		}
 		
 		ModelAndView mav = new ModelAndView();
